e34db201b3de949f5ebb73459b7a3f7315b87b2e
ThallisAlbuquerque <thallis.albu@gmail.com>
2022-10-27 20:33:50 -0300

    portfolio

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var menu_celular = document.querySelector('.menu-celular')
 var menu_h = document.querySelector('.menu-h')
+var logo = document.querySelector('.logo')
 
 function aparecerMenu(){
     if (menu_celular.style.display == 'none'){
         menu_h.innerHTML = 'close'
         menu_celular.style.display = 'block'
+        logo.style.alignSelf = 'flex-start'
     } else{
         menu_h.innerHTML = 'menu'
         menu_celular.style.display = 'none'
+        logo.style.alignSelf = 'center'
     }
 }
 
@@ -16,10 +19,12 @@ function ajeitarMenu(){
     if (window.innerWidth >= 865){
         menu_celular.style.display = 'none'
         menu_h.style.display = 'none'
+        logo.style.alignSelf = 'center'
     } else{
         menu_celular.style.display = 'none'
         menu_h.style.display = 'block'
         menu_h.innerHTML = 'menu'
+        logo.style.alignSelf = 'center'
     }
 }
 
